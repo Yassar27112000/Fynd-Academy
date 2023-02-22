726b1fef9a8039a789d0d76158858eacb59f6c6d
Yassar <klaraib329@gmail.com>
2023-02-22 13:19:14 +0530

    file

---
+++ sum1.java
@@ -0,0 +1,13 @@
+package git_demo;
+
+public class sum1 {
+    public static void main(String[]args){
+        int x;
+        int y;
+        int z;
+        x=2;
+        y=3;
+        z=x+y;
+        System.out.println("Sum of the numbers is "+z);
+     }
+}
